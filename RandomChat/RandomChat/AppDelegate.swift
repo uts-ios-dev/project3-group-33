0682fc4170cc35b1f971335c29e5d909ee485da3
Tony <bestezrealvn@gmail.com>
2018-05-16 02:02:46 +1000

    Firebase configured, ready to use

--- RandomChat/RandomChat/AppDelegate.swift
+++ RandomChat/RandomChat/AppDelegate.swift
@@ -14,8 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+        -> Bool {
+            FirebaseApp.configure()
+            return true
+            
+            
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -39,13 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(_ application: UIApplication,
-                         didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
-            -> Bool {
-                FirebaseApp.configure()
-                return true
-        
     
-    }
 }
 
