cbb151aa69b3b0d07bb4c4ea134dd42891fcaf75
Trevor <trevor@Trevors-iMac.local>
2018-05-15 18:41:33 +1000

    new RandomChat dir

---
+++ RandomChat/RandomChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomChat
+//
+//  Created by Trevor on 12/5/18.
+//  Copyright © 2018 toantt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
