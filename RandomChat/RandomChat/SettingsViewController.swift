ccac64a72fbd8dda235657c9f58176c4af64a122
Trevor <trevor@Trevors-iMac.local>
2018-05-21 17:46:44 +1000

    create several new view controllers

---
+++ RandomChat/RandomChat/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  RandomChat
+//
+//  Created by Trevor on 21/5/18.
+//  Copyright © 2018 toantt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.gray
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        //saveUserbtn.addTarget(self, action: #selector(saveUser), for: Touchs)
+        //ref = Database.database().reference(fromURL: "https://randomchat-a2052.firebaseio.com/")
+        //ref.updateChildValues(["something" : 123]) // example writing data
+        
+    }
+}
