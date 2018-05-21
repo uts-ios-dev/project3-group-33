86e8e0133f8c774112c2c72d9113ddf7a126abcb
Tony <bestezrealvn@gmail.com>
2018-05-21 16:09:28 +1000

    anonymous user implemented

--- RandomChat/RandomChat/ViewController.swift
+++ RandomChat/RandomChat/ViewController.swift
@@ -13,20 +13,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var userNameTxt: UITextField!
     @IBOutlet weak var saveUserbtn: UIButton!
-
+    
     var ref: DatabaseReference!
+    var userUid: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //saveUserbtn.addTarget(self, action: #selector(saveUser), for: Touchs)
-        ref = Database.database().reference(fromURL: "https://randomchat-a2052.firebaseio.com/")
-        ref.updateChildValues(["something" : 123]) // example writing data
+        //ref = Database.database().reference(fromURL: "https://randomchat-a2052.firebaseio.com/")
+        //ref.updateChildValues(["something" : 123]) // example writing data
     
     }
-
     
     
-
+    @IBAction func saveUser(_ sender: UIButton) {
+        Auth.auth().signInAnonymously() { (user, error) in
+            print("Signed in anonymously")
+            let uid = user!.user.uid
+        }
+    }
+    
+    
 }
-
