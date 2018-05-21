340037b8d8740a07d5dbb0ebd767c5ce87ed5506
Trevor <trevor@Trevors-iMac.local>
2018-05-21 16:09:29 +1000

    new storyboard

--- RandomChat/RandomChat/ViewController.swift
+++ RandomChat/RandomChat/ViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var userNameTxt: UITextField!
-    @IBOutlet weak var saveUserbtn: UIButton!
+    @IBOutlet weak var createUser: UIButton!
 
     var ref: DatabaseReference!
     
+    @IBAction func createUser(_ sender: UIButton) {
+        Auth.auth().signInAnonymously() { (user, error) in
+            if let error = error {
+                print("Sign in failed:", error.localizedDescription)
+                return
+            }
+            else {
+                print("Sign in with id:", user!.user.uid)
+                let isAnonymous = user!.user.isAnonymous  // true
+                let uid = user!.user.uid
+            }
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        FirebaseApp.configure()
+        //self.view.backgroundColor = UIColor.gray
+        
         // Do any additional setup after loading the view, typically from a nib.
         //saveUserbtn.addTarget(self, action: #selector(saveUser), for: Touchs)
         ref = Database.database().reference(fromURL: "https://randomchat-a2052.firebaseio.com/")
