820dc5eab8ff72e3d2061c6bd8dd95989df41b7e
Tony <bestezrealvn@gmail.com>
2018-06-07 14:45:42 +1000

    chat room storyboard and class committed

---
+++ RandomChat/RandomChat/ChatRoomViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChatRoomViewController.swift
+//  RandomChat
+//
+//  Created by Lương Linh on 7/6/18.
+//  Copyright © 2018 toantt. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
